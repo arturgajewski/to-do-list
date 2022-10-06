cf3ac87bca74c1d4af49433f4dde414e493bd61b
Artur Gajewski <arturgaj6@wp.pl>
2022-10-06 16:44:57 +0200

    toggle done task

--- js/script.js
+++ js/script.js
@@ -10,18 +10,14 @@
     },
   ];
 
-  const render = () => {
-    let htmlString = "";
-    for (const task of tasks) {
-      htmlString += `
-        <li 
-        ${task.done ? 'style="text-decoration: line-through"' : ""}
-        >
-        ${task.content}
-        </li>
-        `;
-    }
-    document.querySelector(".js-tasks").innerHTML = htmlString;
+  const removeTask = (taskIndex) => {
+    tasks.splice(taskIndex, 1);
+    render();
+  };
+
+  const doneTask = (taskIndex) => {
+    tasks[taskIndex].done = !tasks[taskIndex].done;
+    render();
   };
 
   const addNewTasks = (newTaskContent) => {
@@ -30,6 +26,37 @@
     });
     render();
   };
+
+  const render = () => {
+    let htmlString = "";
+    for (const task of tasks) {
+      htmlString += `
+        <li 
+        ${task.done ? 'style="text-decoration: line-through"' : ""}
+        >
+        <button class="js-done"> zrobione </button>
+        <button class="js-remove"> usuń </button>
+        ${task.content}
+        </li>
+        `;
+    }
+    document.querySelector(".js-tasks").innerHTML = htmlString;
+
+    const removeButtons = document.querySelectorAll(".js-remove");
+    removeButtons.forEach((removeButton, index) => {
+      removeButton.addEventListener("click", () => {
+        removeTask(index);
+      });
+    });
+
+    const doneButtons = document.querySelectorAll(".js-done");
+    doneButtons.forEach((doneButton, index) => {
+      doneButton.addEventListener("click", () => {
+        doneTask(index);
+      });
+    });
+  };
+
   const onFormSubmit = (event) => {
     event.preventDefault();
 

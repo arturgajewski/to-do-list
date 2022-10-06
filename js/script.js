2a2b3b2f599cf4e1693219dab3aa6dd5e812e594
Artur Gajewski <arturgaj6@wp.pl>
2022-10-06 16:20:12 +0200

    adding new tasks and showing if a task is done

--- js/script.js
+++ js/script.js
@@ -1,30 +1,51 @@
 {
-    const tasks = [
-        {
-            content: "odrobić zadanie domowe",
-            done: false,
+  const tasks = [
+    {
+      content: "odrobić zadanie domowe",
+      done: false,
+    },
+    {
+      content: "wysłać pracę domową na slack",
+      done: true,
+    },
+  ];
 
-        }, 
-        {
-            content: "wysłać pracę domową na slack",
-            done: true,
-        },
-    ];
-
-const render = () => {
-    let htmlString= "";
-    for(const task of tasks ){
-        htmlString += `
-        <li>
+  const render = () => {
+    let htmlString = "";
+    for (const task of tasks) {
+      htmlString += `
+        <li 
+        ${task.done ? 'style="text-decoration: line-through"' : ""}
+        >
         ${task.content}
         </li>
         `;
     }
-    document.querySelector(".js-tasks").innerHTML= htmlString;
-};
+    document.querySelector(".js-tasks").innerHTML = htmlString;
+  };
 
-    const init= () => {
-render();
-    };
-    init();
+  const addNewTasks = (newTaskContent) => {
+    tasks.push({
+      content: newTaskContent,
+    });
+    render();
+  };
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const newTaskContent = document.querySelector(".js-newTask").value.trim();
+    if (newTaskContent === "") {
+      return;
+    }
+    addNewTasks(newTaskContent);
+  };
+
+  const init = () => {
+    render();
+
+    const form = document.querySelector(".js-form");
+
+    form.addEventListener("submit", onFormSubmit);
+  };
+  init();
 }
